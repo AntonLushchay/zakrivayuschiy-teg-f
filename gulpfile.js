a8e90c742a31c8c07ae4a79f384f32e7d216f854
Anton Lushchay <av.lushchay17@gmail.com>
2024-05-31 02:43:48 +0200

    first settings for Pug

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,34 @@ const plumber = require('gulp-plumber');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
-const htmlMinify = require('html-minifier');
-function html() {
-    const options = {
-        removeComments: true,
-        removeRedundantAttributes: true,
-        removeScriptTypeAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        sortClassName: true,
-        useShortDoctype: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        keepClosingSlash: true,
-    };
-    return src('src/**/*.html')
-        .pipe(plumber())
-        .on('data', function (file) {
-            const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options));
-            return (file.contents = buferFile);
-        })
+// const htmlMinify = require('html-minifier');
+// function html() {
+//     const options = {
+//         removeComments: true,
+//         removeRedundantAttributes: true,
+//         removeScriptTypeAttributes: true,
+//         removeStyleLinkTypeAttributes: true,
+//         sortClassName: true,
+//         useShortDoctype: true,
+//         collapseWhitespace: true,
+//         minifyCSS: true,
+//         keepClosingSlash: true,
+//     };
+//     return src('src/**/*.html')
+//         .pipe(plumber())
+//         .on('data', function (file) {
+//             const buferFile = Buffer.from(htmlMinify.minify(file.contents.toString(), options));
+//             return (file.contents = buferFile);
+//         })
+//         .pipe(dest('dist/'))
+//         .pipe(browserSync.reload({ stream: true }));
+// }
+
+const gulpPug = require('gulp-pug');
+function pug() {
+    return src('src/pages/**/*.pug')
+        .pipe(gulpPug({ pretty: true }))
+        .pipe(concat('bundle.html'))
         .pipe(dest('dist/'))
         .pipe(browserSync.reload({ stream: true }));
 }
@@ -57,17 +66,20 @@ function clean() {
 function watchFiles() {
     browserSync.init({
         server: {
-            baseDir: './dist',
+            baseDir: 'dist/',
+            index: 'bundle.html',
         },
     });
-    watch(['src/**/*.html'], html);
+    // watch(['src/**/*.html'], html);
+    watch(['src/pages/**/*.pug'], pug);
     watch(['src/**/*.scss'], scssToCss);
     watch(['src/**/*.js'], scripts);
 }
 
-const build = series(clean, parallel(html, scssToCss, scripts));
+const build = series(clean, parallel(pug, scssToCss, scripts));
 
-exports.html = html;
+// exports.html = html;
+exports.pug = pug;
 exports.scssToCss = scssToCss;
 exports.scripts = scripts;
 exports.watchFiles = watchFiles;
